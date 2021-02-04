3f20e5ecc6d7487b4baa52a1870b5c988fd2545f
Greg <gergodemeny@gmail.com>
2021-02-04 10:37:53 +0200

    Update version control

--- builder.js
+++ builder.js
@@ -1,8 +1,13 @@
 const { exec } = require('pkg'),
   pac = require('./package.json');
 
+const getVersion = (version) => {
+  const a = version.split('.');
+  return `${a[0]}.${a[1]}${a[2]}`;
+};
+
 (async () => {
   console.log('Building executables.');
-  await exec(['--output', `hidegvan-${pac.version}`, '.']);
+  await exec(['--output', `hidegvan-${getVersion(pac.version)}`, '.']);
   console.log('Complete.');
 })();
